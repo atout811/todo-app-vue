34895bdd2ab558255f327b630d166fcb850f77bf
atout811 <dmcgfx811@gmail.com>
2020-05-06 00:50:30 +0200

    This is a new commit for what I originally planned to be amended

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vuex from "vuex";
+import Vue from "vue";
+
+import todos from "./modules/todos";
+
+//load vuex
+
+Vue.use(Vuex);
+
+//Create store
+
+export default new Vuex.Store({
+  modules: {
+    todos,
+  },
+});
